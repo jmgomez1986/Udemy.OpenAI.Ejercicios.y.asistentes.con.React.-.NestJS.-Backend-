10e0a10238a20f7958eecf5af978dd6863d572d1
Juan Matias Gomez Andersen <juanmatias.gomez@globant.com>
2026-02-19 10:10:56 -0300

    Seccion 13: Se deja la opcion del uso de toFile de OpenAi para la generacion de imagenes en una edicion, por ser mas performante y propio de OpenAI

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -19,7 +19,7 @@ const getGeneratedImage = async (openai: OpenAI, prompt: string) => {
   });
   const responseUrl = response.data?.[0].url || '';
 
-  // TODO: save image in file system
+  // Save image in file system
   const fileName = await downloadImageAsPng(responseUrl);
   const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
 
@@ -41,20 +41,15 @@ const getEditedImage = async (
   const pngImagePath = await downloadImageAsPng(originalImage, true);
   const maskPath = await downloadBase64ImageAsPng(maskImage, true);
 
-  const imageBuffer = fs.readFileSync(pngImagePath);
-  const maskBuffer = fs.readFileSync(maskPath);
-  const imageFile = new File([imageBuffer], 'image.png', {
-    type: 'image/png',
-  });
-  const maskFile = new File([maskBuffer], 'mask.png', {
-    type: 'image/png',
-  });
-
   const response = await openai.images.edit({
     model: 'dall-e-2',
     prompt,
-    image: imageFile, // fs.createReadStream(pngImagePath
-    mask: maskFile, // fs.createReadStream(maskPath),
+    image: await toFile(fs.createReadStream(pngImagePath), null, {
+      type: 'image/png',
+    }), // fs.createReadStream(pngImagePath),
+    mask: await toFile(fs.createReadStream(maskPath), null, {
+      type: 'image/png',
+    }), // fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
     response_format: 'url',
